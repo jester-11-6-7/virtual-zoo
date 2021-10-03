e3d8500faeefd0157883799095fc14445f0906f7
Jester <vacuum135531@gmail.com>
2021-10-04 00:46:03 +0300

    fit: home page 1200px, ref: slider(js)

--- script/map.js
+++ script/map.js
@@ -16,67 +16,76 @@ function test(name) {
 };
 
 // sliderFn
-function slider() {
+function sliderFn() {
     let offset = 0
 
-    return function (direction, sliderChildren, sliderChildrenGap = 0, slider, columns = 1) {
+    return function (direction, sliderChildren, sliderChildrenGap = 0, sliderContainer, slider, columns = 1,) {
+        let row
+        for (let i = 1; i < 10; i++) {
+            if ((sliderChildren[0].offsetWidth * i) + (sliderChildrenGap * (i - 1)) === slider.offsetWidth) {
+                row = i
+            }
+        }
+
         let sliderElemsWidth = +(direction + sliderChildren[0].offsetWidth),
             sliderContainerGap = +(direction + parseFloat(sliderChildrenGap)),
             sliderWidth = (
-                ((Math.floor(sliderChildren.length / columns) - columns) * sliderChildrenGap) +
-                (sliderChildren[0].offsetWidth * Math.floor(sliderChildren.length / columns))) +
-                ((sliderContainerGap * columns) - (sliderChildren[0].offsetWidth * columns)
+                ((Math.ceil(sliderChildren.length / columns) - row) * sliderChildrenGap) +
+                (sliderChildren[0].offsetWidth * Math.ceil(sliderChildren.length / columns))) -
+                (sliderChildren[0].offsetWidth * (row === 1 ? row : row - 1)
                 ),
             oneShift = sliderElemsWidth + sliderContainerGap;
 
-        if (offset + oneShift < 1 && offset + oneShift > -sliderWidth) {
-            slider.style.transform = `translateX(${offset += oneShift}px)`
+        if (offset + oneShift < 1 && offset + oneShift >= -sliderWidth) {
+            sliderContainer.style.transform = `translateX(${offset += oneShift}px)`
         }
     }
 };
 
 !function reviewsSliderFn() {
-    const btn = document.querySelector('.testimonials__btn-slider').children
-    const sliderContainer = document.querySelector('.testimonials__slider-bar')
-    const sliderElems = document.querySelectorAll('.testimonials__user-reviews')
-    let reviewsSlider = slider()
+    const btn = document.querySelector('.testimonials__btn-slider').children,
+        reviewsSlider = sliderFn(),
+        derection = ['+', '-']
 
-    btn[0].addEventListener('click', () => reviewsSlider(
-        '+',
-        sliderElems,
-        parseFloat(window.getComputedStyle(sliderContainer).gap),
-        sliderContainer,
-    ))
-
-    btn[1].addEventListener('click', () => reviewsSlider(
-        '-',
-        sliderElems,
-        parseFloat(window.getComputedStyle(sliderContainer).gap),
-        sliderContainer
-    ))
+    for (let i = 0; i < btn.length; i++) {
+        btn[i].addEventListener('click', () => {
+            const slider = document.querySelector('.testimonials__slider'),
+                sliderContainer = document.querySelector('.testimonials__slider-bar'),
+                sliderElems = document.querySelectorAll('.testimonials__user-reviews');
+            reviewsSlider(
+                derection[i],
+                sliderElems,
+                parseFloat(window.getComputedStyle(sliderContainer).gap),
+                sliderContainer,
+                slider,
+            )
+        })
+    }
 }();
+
+
 !function animalSliderFn() {
-    const btn = document.querySelector('.famous-pets__btn-slider').children
-    const sliderContainer = document.querySelector('.famous-pets__slider-bar')
-    const sliderElems = document.querySelectorAll('.famous-pets__animal')
-    let animalSlider = slider()
+    const btn = document.querySelector('.famous-pets__btn-slider').children,
+        animalSlider = sliderFn(),
+        derection = ['+', '-']
 
-    btn[0].addEventListener('click', () => animalSlider(
-        '+',
-        sliderElems,
-        parseFloat(window.getComputedStyle(sliderContainer).gap),
-        sliderContainer,
-        2
-    ))
-
-    btn[1].addEventListener('click', () => animalSlider(
-        '-',
-        sliderElems,
-        parseFloat(window.getComputedStyle(sliderContainer).gap),
-        sliderContainer,
-        2
-    ))
+    for (let i = 0; i < btn.length; i++) {
+        btn[i].addEventListener('click', () => {
+            const slider = document.querySelector('.famous-pets__slider'),
+                sliderContainer = document.querySelector('.famous-pets__slider-bar'),
+                sliderElems = document.querySelectorAll('.famous-pets__animal');
+            animalSlider(
+                derection[i],
+                sliderElems,
+                parseFloat(window.getComputedStyle(sliderContainer).gap),
+                sliderContainer,
+                slider,
+                2,
+            )
+        })
+    }
 }();
+
 
 // card animation
 !function animateCardAnimal() {
